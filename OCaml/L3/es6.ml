8d9f83eb381aad8180acdca1f6a7aa21adba6171
michelebolis <mickbolis@gmail.com>
2023-10-11 10:01:25 +0200

    Trigonometric module done

--- OCaml/L3/es6.ml
+++ OCaml/L3/es6.ml
@@ -10,3 +10,31 @@ Do the same with cosine, tangent, logarithm and so on.
 
 Let's compare your functions with those implemented in the pervasive module at the growing of the approximation level.   
 *)
+module Trigonometric : 
+  sig
+    val sin : float -> int -> float
+  end
+  = 
+  struct 
+    let rec fact = function
+      | 1 -> 1
+      | x -> x * (fact (x - 1))
+    ;;
+
+    let rec pow x = function
+      | 0 -> 1.0
+      | i -> x *. (pow x (i-1))
+    ;;
+
+    let sin x n = 
+      if(x = 0.0) then 0.0 else
+      let rec sin_add = function
+        | 1 -> x
+        | i -> if(i mod 2==0) then
+                let exp = ((i*2)-1) in -.(pow x exp /. (float (fact exp))) +. sin_add (i-1)
+              else
+                let exp = ((i*2)-1) in (pow x exp /. (float (fact exp))) +. sin_add (i-1)
+      in sin_add n
+    ;;
+  end
+;;
