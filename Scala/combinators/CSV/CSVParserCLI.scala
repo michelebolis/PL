7cc6b748599d4fe14f4f0f2f10af4a3e9d019bc3
michelebolis <mickbolis@gmail.com>
2024-01-05 15:57:56 +0100

    CSVParserCLI print formatted

--- Scala/combinators/CSV/CSVParserCLI.scala
+++ Scala/combinators/CSV/CSVParserCLI.scala
@@ -1,4 +1,5 @@
 import scala.util.parsing.combinator._
+import scala.math._
 /* 
     1. is plain text using a character set such as ASCII or Unicode,
     2. consists of records (one record per line),
@@ -16,10 +17,49 @@ import scala.util.parsing.combinator._
 class CSVParser extends JavaTokenParsers {
     override val skipWhitespace = false
     override val whiteSpace = """[ \t\n]""".r
-    def start : Parser[List[List[String]]] = record ~ rep("""\r\n""".r ~> record) ^^ {case r1 ~ l => List(r1):::l}
+    def start : Parser[Unit] = record ~ rep("""\r\n""".r ~> record) ^^ {case r1 ~ l => printTable(List(r1):::l)}
     def record : Parser[List[String]] = field ~ "," ~ record ^^ 
         {case first ~ _ ~ fields => List(first):::fields} | field ^^ {f => List(f)}
     def field : Parser[String] = "[^\r\n\t,]*".r ^^ {s => s.toString} 
+
+    def printTable(l : List[List[String]]) : Unit = {
+        l match {
+            case t::tl =>
+                val sizes = getMaxSizes(l)
+                var totSizes = t.length + 1
+                for (k <- 0 to sizes.length-1) totSizes += sizes(k) + 2
+                val dashes = "-" * totSizes
+                println(getRowString(t, sizes))
+                println(dashes)
+                tl.foreach{row =>
+                    println(getRowString(row, sizes))
+                }
+                println(dashes)
+            case x =>
+        }
+    }
+    def getMaxSizes(l : List[List[String]]) : Array[Int] = {
+        var sizes = new Array[Int](l(1).length)
+        l.foreach{ row =>
+            var i = 0
+            row.foreach{ field =>
+                sizes(i) = max(sizes(i), field.length)
+                i += 1
+            }
+        }
+        sizes
+    }
+    def getRowString(l : List[String], sizes : Array[Int]) : String = {
+        var col = 0
+        var s = "| "
+        l.foreach{ field =>
+            s += field
+            for(k <- 1 to (sizes(col) - field.length)) s += " " 
+            s += " | "
+            col += 1
+        }
+        return s
+    }
 }
 
 object Main {
@@ -29,7 +69,7 @@ object Main {
             val lines = src.mkString 
             val p = new CSVParser
             p.parseAll(p.start, lines) match {
-                case p.Success(t, _) => println(t)
+                case p.Success(t, _) => 
                 case x => print(x.toString)
             }
         }
